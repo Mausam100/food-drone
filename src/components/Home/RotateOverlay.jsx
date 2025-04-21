b15c4ab5c4b73b60f765852d72e6aa3c180c1c29
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-04-21 13:07:04 +0530

    Mobile Rotate Overlay UI Added

--- src/components/Home/RotateOverlay.jsx
+++ src/components/Home/RotateOverlay.jsx
@@ -4,6 +4,11 @@ const RotateOverlay = ({ toggleFullScreen, lockOrientation }) => {
   return (
     <div className="fixed inset-0 z-[2000] flex items-center justify-center bg-black/80 backdrop-blur-md p-4">
       <div className="bg-white/10 border border-white/20 rounded-2xl shadow-xl px-8 py-10 flex flex-col items-center max-w-sm w-full">
+        <div className="mb-2">
+          <span className="text-2xl md:text-3xl font-extrabold tracking-wide text-white drop-shadow-lg">
+            Rotate Device
+          </span>
+        </div>
         <div className="mb-4">
           <img
             src="/rotate-phone.gif"
@@ -11,16 +16,11 @@ const RotateOverlay = ({ toggleFullScreen, lockOrientation }) => {
             className="w-35 block md:hidden h-auto"
           />
         </div>
-        <div className="mb-2">
-          <span className="text-2xl md:text-3xl font-extrabold tracking-wide text-white drop-shadow-lg">
-            Rotate Device
-          </span>
-        </div>
         <div className="mb-6">
-          <span className="text-base md:text-lg text-gray-200 font-medium">
+          <p className="text-base md:text-lg text-gray-200 font-medium text-center">
             For the best experience, please rotate your phone to{" "}
             <span className="text-[#00c3ae] font-bold">landscape</span> mode.
-          </span>
+          </p>
         </div>
         <button
           onClick={() => {
